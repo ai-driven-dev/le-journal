8536e4a1c1904aa85772d41e2a76add9038e2bd3
alexsoyes <contact.alexsoyes@gmail.com>
2025-02-02 16:48:05 +0100

    ci: configure github actions to run on main branch only

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,7 +4,19 @@ export default {
     'type-enum': [
       2,
       'always',
-      ['fix', 'build', 'feat', 'docs', 'chore', 'refactor', 'test', 'BREAKING CHANGE'],
+      [
+        'build', // Changes that affect the build system or external dependencies
+        'ci', // Changes to our CI configuration files and scripts
+        'docs', // Documentation only changes
+        'feat', // A new feature
+        'fix', // A bug fix
+        'perf', // A code change that improves performance
+        'refactor', // A code change that neither fixes a bug nor adds a feature
+        'revert', // Reverts a previous commit
+        'style', // Changes that do not affect the meaning of the code (white-space, formatting, etc)
+        'test', // Adding missing tests or correcting existing tests
+        'chore', // Other changes that don't modify src or test files
+      ],
     ],
   },
 };
